6b32de7ecf6a35b2c10112ea2b4ee5076eb055f5
cwwjie <454766952@qq.com>
2017-10-26 17:34:49 +0800

    add Python

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Restful API - 第三版
+# Restful API - 第三期 Python版 
 
-  
+### 如何启动?
+
+$ python manage.py runserver 127.0.0.1:8193 (ip:port)
 
--- controller/slider/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('koa-router')();
-const getAllImg = require('./getAllImg');
-
-router.get('/get', getAllImg);
-router.get('/',(ctx, next)=> {
-    ctx.body = 'you get /slider route';
-});
-
-module.exports = router;
